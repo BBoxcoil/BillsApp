654e9ffcee35a0bd15a449d32d7e47c344aefd71
Michael Abramovich <4584975@gmail.com>
2013-09-07 20:44:21 +0100

    add RTL user pages support

--- public/users/js/views/signup.js
+++ public/users/js/views/signup.js
@@ -1,4 +1,4 @@
-
+﻿
 $(document).ready(function(){
 	
 	var av = new AccountValidator();
@@ -23,17 +23,17 @@ $(document).ready(function(){
 	
 // customize the account signup form //
 	
-	$('#account-form h1').text('Signup');
-	$('#account-form #sub1').text('Please tell us a little about yourself');
-	$('#account-form #sub2').text('Choose your username & password');
-	$('#account-form-btn1').html('Cancel');
-	$('#account-form-btn2').html('Submit');
+	$('#account-form h1').text('יצירת חשבון');
+	$('#account-form #sub1').text('נא ספר לנו קצת על אצמך');
+	$('#account-form #sub2').text('בחר שם וסיסמתך');
+	$('#account-form-btn1').html('ביטול');
+	$('#account-form-btn2').html('צור חשבון');
 	$('#account-form-btn2').addClass('btn-primary');
 	
 // setup the alert that displays when an account is successfully created //
 
 	$('.modal-alert').modal({ show : false, keyboard : false, backdrop : 'static' });
-	$('.modal-alert .modal-header h3').text('Success!');
+	$('.modal-alert .modal-header h3').text('הצלחה!');
 	$('.modal-alert .modal-body p').html('Your account has been created.</br>Click OK to return to the login page.');
 
 })
